65b9c121b45d270981f84473426f35b9b2887d73
cuidayede <coloureye@163.com>
2024-04-17 10:53:46 +0800

    from think

--- ch01-人工智能绪论/autograd.py
+++ ch01-人工智能绪论/autograd.py
@@ -1,4 +1,13 @@
-import tensorflow as tf 
+"""
+该代码主要作用是使用TensorFlow库进行自动微分。主要计算过程可以用以下数学公式表示：
+假设有一个二次函数 y = a * w**2 + b * w + c，其中 a = 1, b = 2, c = 3, w = 4。
+想要计算函数 y 关于 w 的导数，即 dy/dw。根据二次函数的导数公式，dy/dw = 2*a*w + b
+将 a = 1, b = 2, w = 4 代入上述公式，得到：dy/dw = 2*1*4 + 2 = 10 这就是主要计算过程。
+使用TensorFlow的 tf.GradientTape() 上下文和 tape.gradient() 函数自动完成了这个导数计算过程。
+在脚本的最后，它打印出了计算得到的导数值 dy/dw = 10。
+"""
+
+import tensorflow as tf
 
 # 创建4个张量
 a = tf.constant(1.) #python中1.和1.0都表示相同的浮点数值1.0。写成1.的方式是一种简写形式，
